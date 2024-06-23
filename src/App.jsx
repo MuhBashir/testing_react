e746b7d5bc80ba10f2fd80a5f5ba1445b924cf7b
Muhammad Bashir Ibrahim <mbashiribrahim7@gmail.com>
2024-06-23 16:27:37 +0100

    Adding Starter files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from './components/Home';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <main>
+      <Home />
+      <Skills />
+      <Projects />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
